163b0dac13767279990f603d957375fe3de2ba70
Prassadraj <prassadpr10@gmail.com>
2024-08-16 15:53:38 +0530

    type-php

--- src/components/Management/Management.jsx
+++ src/components/Management/Management.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./manage.css";
 import { PieChart } from "@mui/x-charts";
 import {
@@ -7,43 +7,45 @@ import {
   faComment,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 function Management() {
-  const [isNotificationsVisible, setIsNotificationsVisible] = useState(false);
-
   const data = [
     { id: 0, value: 10, label: "Series A" },
     { id: 1, value: 15, label: "Series B" },
     { id: 2, value: 20, label: "Series C" },
   ];
-
-  const toggleNotifications = () => {
-    setIsNotificationsVisible(!isNotificationsVisible);
-  };
+  const [type, setType] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost/AMS/backend/fetchTicket_type.php"
+        );
+        setType(response.data);
+      } catch (error) {}
+      setError(error);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="lg:flex p-4 w-full h-screen font-poppins lg:grid-cols-2 grid-cols-1">
       {/* Sidebar */}
       <div className="p-4 h-full bg-slate-300 w-full lg:w-2/4 rounded-md flex justify-center border-r-2">
         <div className="grid grid-cols-2 gap-4 h-fit text-xl mt-5 w-full">
-          <p className="p-2 w-full border text-center rounded-lg bg-white text-violet-500 cursor-pointer">
-            IT
-          </p>
-          <p className="p-2 text-center w-full border rounded-lg bg-white cursor-pointer">
-            Non-IT
-          </p>
-          <p className="p-2 w-full border text-center rounded-lg bg-white text-violet-500 cursor-pointer">
-            IT
-          </p>
-          <p className="p-2 text-center w-full border rounded-lg bg-white cursor-pointer">
-            Non-IT
-          </p>
-          <p className="p-2 w-full border text-center rounded-lg bg-white text-violet-500 cursor-pointer">
-            IT
-          </p>
-          <p className="p-2 text-center w-full border rounded-lg bg-white cursor-pointer">
-            Non-IT
-          </p>
+          {type.map(
+            (item, i) =>
+              item.group_id == 1 && (
+                <p
+                  key={i}
+                  className="p-2 text-violet-700 text-center w-full border rounded-lg bg-white cursor-pointer"
+                >
+                  {item.type}
+                </p>
+              )
+          )}
         </div>
       </div>
 
@@ -69,10 +71,7 @@ function Management() {
           </div>
         </div>
         <div className="w-full md:w-2/4 flex flex-col gap-2 h-full px-4 py-4 overflow-hidden">
-          <div
-            className="flex items-center cursor-pointer"
-            onClick={toggleNotifications}
-          >
+          <div className="flex items-center ">
             <div className="relative inline-block">
               <FontAwesomeIcon
                 icon={faBell}
@@ -80,7 +79,7 @@ function Management() {
               />
               <div
                 style={{ borderRadius: "100%" }}
-                className="absolute -top-3 -right-2 bg-red-500 px-1  flex items-center justify-center text-xs"
+                className="absolute -top-4 left-2 bg-[red] px-1  flex items-center justify-center text-xs"
               >
                 <p className="text-white text-sm">1</p>
               </div>
@@ -91,15 +90,11 @@ function Management() {
             </p>
             <FontAwesomeIcon
               icon={faChevronDown}
-              className={`transition-transform duration-300 ${
-                isNotificationsVisible ? "rotate-180" : ""
-              }`}
+              className={`transition-transform duration-300 `}
             />
           </div>
           <div
-            className={`scrollbar-thin overflow-y-scroll p-4 bg-second rounded-md shadow-lg transition-transform duration-500 ${
-              isNotificationsVisible ? "translate-y-0" : "translate-y-full"
-            }`}
+            className={`scrollbar-thin overflow-y-scroll p-4 bg-second rounded-md shadow-lg transition-transform duration-500 `}
           >
             <p className="border-b border-gray-500 py-2 flex items-center">
               <FontAwesomeIcon icon={faComment} className="mr-2" /> Notification
